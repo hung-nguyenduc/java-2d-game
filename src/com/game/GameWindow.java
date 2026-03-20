b9eb9c89d5802b287098c51b61904900af94106b
Nguyen Duc Hung <harukieatsuki@gmail.com>
2026-03-20 15:57:18 +0700

    delete photos in player due to merging problems

--- src/com/game/GameWindow.java
+++ src/com/game/GameWindow.java
@@ -5,6 +5,10 @@ import javax.swing.JFrame;
 public class GameWindow {
     public GameWindow() {
         JFrame frame = new JFrame("My Game");
+
+        GamePanel panel = new GamePanel();
+        frame.add(panel);
+
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
