c1965bd859ff46c42f18c419474718969059fd6c
Nguyen Duc Hung <harukieatsuki@gmail.com>
2026-04-12 11:45:21 +0700

    cấu trúc lại folder

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,21 @@
 package main;
 
+import javax.swing.*;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame("Sinh ton o HUST");
+        frame.setVisible(true);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+    }
 }
