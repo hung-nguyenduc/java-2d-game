0a6fa886540dce402e4d4afcdd752ffce2e95fad
dungvippro2711 <tridungtuki@gmail.com>
2026-05-07 18:17:33 +0700

    i

---
+++ src/main/gamestatedoubke.java
@@ -0,0 +1,9 @@
+package main;
+
+public interface gamestatedoubke {
+    public void update();
+    public void draw(java.awt.Graphics2D g2);
+    public void keyPressed(int k);
+    public void keyReleased(int k); 
+    
+}
