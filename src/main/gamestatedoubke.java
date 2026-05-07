86ad7836a993fc52cc8c1d62c3df6faedfec125e
dungvippro2711 <tridungtuki@gmail.com>
2026-05-07 18:20:22 +0700

    xóa file lỗi

--- src/main/gamestatedoubke.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public interface gamestatedoubke {
-    public void update();
-    public void draw(java.awt.Graphics2D g2);
-    public void keyPressed(int k);
-    public void keyReleased(int k); 
-    
-}
