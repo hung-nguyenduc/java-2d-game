3006915135660c46ed8d23c4c742d4aefe3b7dca
Nguyen Duc Hung <harukieatsuki@gmail.com>
2026-05-26 14:40:33 +0700

    update comment

--- src/state/GameState.java
+++ src/state/GameState.java
@@ -30,7 +30,6 @@ public abstract class GameState {
     // Handle mouse clicks (e.g., for menu buttons)
     public abstract void handleMouseClick(MouseEvent e);
 
-    // Thêm hàm này vào file GameState.java
     public java.util.List<Obstacle> getObstacles() {
         return new java.util.ArrayList<>();
     }
