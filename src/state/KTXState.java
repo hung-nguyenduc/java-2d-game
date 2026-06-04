b9682b3b98630f057713292f6eaefbb6daaa28fa
Nguyen Duc Hung <harukieatsuki@gmail.com>
2026-06-04 11:20:43 +0700

    remove image background

--- src/state/KTXState.java
+++ src/state/KTXState.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KTXState extends GameState {
-    private boolean debugMode =  true;
+    private boolean debugMode =  false;
     private static final String MAP_IMAGE_PATH = "/maps/ktx-xoa-balo.png";
     private static final String OBSTACLE_TXT_PATH = "/maps/ktx_obstacles.txt";
     private static final double MAP_SCALE = 1.0 / 2.5;
