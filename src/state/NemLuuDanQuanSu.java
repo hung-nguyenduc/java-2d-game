ae816a7ce93396f02f28701074f6ef22e859cbf8
AQL0701 <marcolego0701@gmail.com>
2026-06-07 23:18:56 +0700

    Them nhac nen cho man Nem Luu Dan Quan Su

--- src/state/NemLuuDanQuanSu.java
+++ src/state/NemLuuDanQuanSu.java
@@ -77,6 +77,7 @@ public class NemLuuDanQuanSu extends GameState {
 
     @Override
     public void enter() {
+        gp.sound.playMusic("nhac_nen_2"); // Nhạc nền màn Ném Lựu Đạn
         try {
             BufferedImage src = ImageIO.read(getClass().getResourceAsStream(MAP_IMAGE_PATH));
             gp.worldWidth = (int) (src.getWidth() * MAP_SCALE);
